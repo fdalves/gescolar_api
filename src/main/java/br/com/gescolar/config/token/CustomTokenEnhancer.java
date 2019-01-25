1c3eaa740a841d5b7384b7a9d5bc939c03ef3026
francisco <francisco.d.alves@gmail.com>
2019-01-25 00:38:58 -0200

    teste

--- src/main/java/br/com/gescolar/config/token/CustomTokenEnhancer.java
+++ src/main/java/br/com/gescolar/config/token/CustomTokenEnhancer.java
@@ -1,25 +1,38 @@
 package br.com.gescolar.config.token;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+import br.com.gescolar.model.Professor;
+import br.com.gescolar.repository.ProfessorRepository;
 import br.com.gescolar.security.UsuarioSistema;
+import br.com.gescolar.types.TipoUsuarioEnum;
 
 
 public class CustomTokenEnhancer implements TokenEnhancer {
 
+	
+	@Autowired
+	private ProfessorRepository professorRepository; 
+	
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		UsuarioSistema usuarioSistema = (UsuarioSistema) authentication.getPrincipal();
-		
 		Map<String, Object> addInfo = new HashMap<>();
 		addInfo.put("tipoUsuario", usuarioSistema.getUsuario().getTipoUsuario());
 		addInfo.put("codigoUsuario", usuarioSistema.getUsuario().getCodigo());
+		
+		if (usuarioSistema.getUsuario().getTipoUsuario().getDescTipoUsuario().equalsIgnoreCase(TipoUsuarioEnum.PROFESSOR.toString())) {
+			List<Professor> list = professorRepository.findByUsuario(usuarioSistema.getUsuario());
+			if (list != null && !list.isEmpty()) addInfo.put("codigoProfessor", list.get(0).getCodigo());
+		}
 		
 		((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(addInfo);
 		return accessToken;
--- src/main/java/br/com/gescolar/repository/ProfessorRepository.java
+++ src/main/java/br/com/gescolar/repository/ProfessorRepository.java
@@ -7,12 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import br.com.gescolar.model.Professor;
+import br.com.gescolar.model.Usuario;
+import java.util.List;
 
 
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long>{
 	
 	public Page<Professor> findByNomeContaining(String nome, Pageable pageable);
+	
+	public List<Professor> findByUsuario(Usuario usuario);
 
 	public boolean existsByCpf(String cpf);
 }
